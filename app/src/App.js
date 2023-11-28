7f944c25cbb6139989593a027d666cba3362b42b
josh <111506176+snff-io@users.noreply.github.com>
2023-11-28 00:03:27 +0000

    react and stuff

---
+++ app/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Z
+      </header>
+      <div>
+        <p>SPECIFICATION</p>
+        <p>Inline Z language elements are comprised of digits 0-9 and the letter Z.
+           Each digit represents discrete converstaion element ("you", "me")
+           Z messages transmit as single-hand sign-language or 32bit floating point numbers allowing for efficent communication line-of-sight, interdimentionally and temporaly asynchronous situations utilizing entanglement. 
+           with additional element affixes implied by context or repetition.
+           For example, 8 denotes time, 88 urgency, 08 lack of, 80 plenty of, 080 none
+
+        </p>
+        
+      </div>
+      <div>
+        <table>
+          <tr>
+            <td>1</td><td></td>
+          </tr>
+
+        </table>
+
+    </div>
+    </div>
+  );
+}
+
+export default App;
